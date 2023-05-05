55262ac5f2caf5276652e6c5fd387093205fea27
Ricardo <ricardohsfaria123@gmail.com>
2023-05-05 13:41:36 -0300

    style: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import Logo from "../assets/images/logo.svg";
+import '../style/Footer.css';
 
 export default function Footer() {
   return (
-    <div>
+    <div className="footer">
         <img src={Logo} alt="smart fit" />
         <p>Todos os direitos reservados - 2023</p>
     </div>
